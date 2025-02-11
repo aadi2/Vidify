f020a51a0c6b09d796c9da8b76893af546669188
Aadi Aggarwal <aadiaggarwal@Aadis-MacBook-Pro-3.local>
2025-02-10 21:24:42 -0500

    changed the formatting which was causing errors in the build

--- src/backend/app.py
+++ src/backend/app.py
@@ -3,15 +3,15 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+
+@app.route("/", methods=["POST"])
 def home():
-    result = {
-        "message": "Request Received."
-    }
+    result = {"message": "Request Received."}
 
     return jsonify(result), 200
 
-if __name__ == '__main__':
-    if not os.path.exists('temp'):
-        os.makedirs('temp')
-    app.run(debug=True, host='0.0.0.0', port=8000)
+
+if __name__ == "__main__":
+    if not os.path.exists("temp"):
+        os.makedirs("temp")
+    app.run(debug=True, host="0.0.0.0", port=8000)
