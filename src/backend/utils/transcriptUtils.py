afe49b0dca413db810c3f4d1a833946d771f18df
okosowska <okosowska333@gmail.com>
2025-03-06 12:35:46 -0500

    Set up transcriptUtils class.

---
+++ src/backend/utils/transcriptUtils.py
@@ -0,0 +1,31 @@
+
+class transcriptUtils():
+
+    """Class constructor
+
+    Args: None
+
+    Returns: None
+    """
+    def __init__():
+        pass
+
+    """Create a transcript if it is not available.
+
+    Args: None
+
+    Returns: None
+    """
+    def create_transcript():
+        # Owner: Ola
+        pass
+
+    """Search the transcript for the keywords.
+
+    Args: None
+
+    Returns: None
+    """
+    def search_transcript():
+        # Owner: Trent
+        pass
