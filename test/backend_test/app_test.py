ef06fb7759807174b8aea0b0194603a71f82d0a0
okosowska <okosowska333@gmail.com>
2025-04-17 12:53:32 -0400

    Allow for longer startup time

--- test/backend_test/app_test.py
+++ test/backend_test/app_test.py
@@ -21,7 +21,7 @@ class TestSuite(unittest.TestCase):
             [sys.executable, "src/backend/app.py"], stdout=sys.stdout, stderr=sys.stderr
         )
 
-        for _ in range(6):
+        for _ in range(15):
             try:
                 response = requests.get(f"{BASE_URL}/health")
                 if response.status_code == 200:
